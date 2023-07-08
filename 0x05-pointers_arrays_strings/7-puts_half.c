0f7e5e6b7d53977fdd7ed58415f332c4666a6c52
Mustaphajafari <jaafarimustapha28@gmail.com>
2023-07-08 13:06:14 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,23 @@
 void puts_half(char *str)
 {
 int length = 0;
-int i, start_index;
+int i;
 
 while (str[length] != '\0')
 {
 length++;
 }
 
-if (length % 2 == 0)
+if (length % 2 == 1)
 {
-start_index = length / 2;
+i = (length - 1) / 2;
+i += 1;
 }
 else
 {
-start_index = (length - 1) / 2;
+i = length / 2;
 }
-for (i = start_index; i < length; i++)
+for (; i < length; i++)
 {
 _putchar(str[i]);
 }
