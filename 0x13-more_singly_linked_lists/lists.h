26956e21fa32396ac510501763cc68f28f9dc77c
mustapha jafari <jaafarimustapha28@gmail.com>
2023-08-01 09:32:30 -0700

    h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * struct listint_s - singly linked list
@@ -21,5 +23,9 @@ struct listint_s *next;
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+
+
 
 #endif
