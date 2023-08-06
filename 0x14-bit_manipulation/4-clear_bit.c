27a56712451037e0bc0a1b7956a746d45b2968b1
mustapha jafari <jaafarimustapha28@gmail.com>
2023-08-06 04:03:16 -0700

    h

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,13 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask;
-if (index >= sizeof(unsigned long int) * 8)
-return (1);
+if (index > 63)
+return (-1);
 
-mask = ~(1UL << index);
-
-*n &= mask;
-
+*n = (~(1UL << index) & *n);
 return (1);
 }
